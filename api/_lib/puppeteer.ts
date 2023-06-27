5faf0af6eb06e129a01189dd1b78619312236324
mrcabbage2020 <111248128+mrcabbage2020@users.noreply.github.com>
2023-06-27 09:39:18 +0800

    Update puppeteer.ts

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -18,6 +18,6 @@ export async function getScreenshot(url, width, height) {
     const page = await getPage();
     await page.goto(url);
     await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 1 });
-    const file = await page.screenshot({fullPage: true });
+    const file = await page.screenshot();
     return file;
 }
